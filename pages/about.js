ffa30320cd65595d71a913b914ae87e8eb28fd05
jalishmahmud <jalish93@gmail.com>
2022-04-02 00:54:05 +0600

    project setup and routing with next done

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const about = () => {
+  return (
+    <div>
+      <Head>
+        <title>About</title>
+      </Head>
+      <h2>About Us page</h2>
+    </div>
+  );
+};
+
+export default about;
